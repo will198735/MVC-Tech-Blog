19d2585db88bbdbf1c602d56950269798087ac37
will198735 <recinoswilfredo@gmail.com>
2023-04-05 23:46:19 -0700

    modified readme file

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # MVC-Tech-Blog
 
 
+
+ ## Table of content
+
+- [Description](#Description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
 ## Description
 This project uses the Model-View-Controller (MVC) paradigm to create a blog-style website where developers can create an account, edit their information, make posts, edit posts, and comment on other user's posts.
+
+
+
+## Installation
+
+
  
 
  ## Usage
+
+
+ ## Licence
+ 
+
+
+
  
--- server.js
+++ server.js
@@ -1,43 +1,43 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
-
-// Create a new sequelize store using the express-session package
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    helpers
+});
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+        checkExpirationInterval: 1000 * 60 * 10, 
+        expiration: 1000 * 60 * 30 
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
-
-// Configure and link a session object with the sequelize store
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-// Add express-session and store as Express.js middleware
-app.use(session(sess));
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
 });
